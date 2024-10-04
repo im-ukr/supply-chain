01ee7f2788cd7b968de181cb80f9d153f58c8ab6
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-10-04 17:53:18 +0530

    simplified click handling

--- frontend/src/components/Vineyard.js
+++ frontend/src/components/Vineyard.js
@@ -1,58 +1,51 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import { Accordion, Icon, Header, Grid } from 'semantic-ui-react';
 import Map from './Map';
-
 import { parseId } from '../controllers/api';
 
 class Vineyard extends Component {
   state = { open: true };
 
   handleClick = () => {
-    const { open } = this.state;
-    this.setState({ open: !open });
+    this.setState(prevState => ({ open: !prevState.open }));
   };
 
   render() {
     const { data } = this.props;
-    if (data.length === 0) return null;
+    if (!data || Object.keys(data).length === 0) return null;
 
     return (
       <div style={{ marginTop: '20px' }}>
         <Accordion>
-          <Accordion.Title
-            active={this.state.open}
-            index={0}
-            onClick={this.handleClick}
-          >
+          <Accordion.Title active={this.state.open} onClick={this.handleClick}>
             <Header as="h1">
               <Icon name="dropdown" />
-              Grapes grown at {parseId(data.grapesData.vineyard)}
+              Grapes grown at {parseId(data.grapesData?.vineyard)}
             </Header>
           </Accordion.Title>
-          <Accordion.Content
-            active={this.state.open}
-            style={{ textAlign: 'left' }}
-          >
-            <Grid columns={3} style={{ fontSize: '1.1rem' }}>
-              <Grid.Column />
-              <Grid.Column>
-                <Grid.Row>Altitude:</Grid.Row>
-                <Grid.Row>Region: </Grid.Row>
-                <Grid.Row>Grape: </Grid.Row>
-              </Grid.Column>
-              <Grid.Column>
-                <Grid.Row>{data.vineyard.altitude}m</Grid.Row>
-                <Grid.Row>{data.vineyard.region}</Grid.Row>
-                <Grid.Row>{data.grapesData.species}</Grid.Row>
-              </Grid.Column>
+          <Accordion.Content active={this.state.open} style={{ textAlign: 'left' }}>
+            <Grid columns={2} divided="vertically" style={{ fontSize: '1.1rem' }}>
+              <Grid.Row>
+                <Grid.Column textAlign="right" width={5}>
+                  <strong>Altitude:</strong>
+                </Grid.Column>
+                <Grid.Column>{data.vineyard.altitude}m</Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column textAlign="right" width={5}>
+                  <strong>Region:</strong>
+                </Grid.Column>
+                <Grid.Column>{data.vineyard.region}</Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column textAlign="right" width={5}>
+                  <strong>Grape:</strong>
+                </Grid.Column>
+                <Grid.Column>{data.grapesData?.species}</Grid.Column>
+              </Grid.Row>
             </Grid>
-            <Map
-              location={[
-                data.vineyard.location.latitude,
-                data.vineyard.location.longitude
-              ]}
-            />
+            <Map location={[data.vineyard.location.latitude, data.vineyard.location.longitude]} />
           </Accordion.Content>
         </Accordion>
       </div>
@@ -60,6 +53,21 @@ class Vineyard extends Component {
   }
 }
 
-Vineyard.propTypes = {};
+Vineyard.propTypes = {
+  data: PropTypes.shape({
+    vineyard: PropTypes.shape({
+      altitude: PropTypes.number,
+      region: PropTypes.string,
+      location: PropTypes.shape({
+        latitude: PropTypes.number,
+        longitude: PropTypes.number,
+      }).isRequired,
+    }).isRequired,
+    grapesData: PropTypes.shape({
+      vineyard: PropTypes.string,
+      species: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Vineyard;
